3acea1c412ebdbf18c1d53796973e5ea7097643c
hitisha-goyani <goyanihitisha@gmail.com>
2025-09-03 12:53:47 +0530

    day-2

--- day-2-all-crud/index.js
+++ day-2-all-crud/index.js
@@ -51,7 +51,7 @@ app.get("/student/:id",(req,res) =>{
         return res.status(404).json("student not found with this id");
   }
 
- res.status(200).json({ message: "student found", student });
+ res.status(200).json({ message: "student found", student});
 
 
 })
@@ -59,7 +59,66 @@ app.get("/student/:id",(req,res) =>{
 
 // add 
 
-app.post("/students/add",(req,res))
+app.post("/student/add",(req,res)=>{
+
+    console.log(req.body.name);
+
+const newStudent = {
+    id:new Date().getTime(),
+    name:req.body,
+}
+
+students.push(newStudent)   
+
+res.status(201).json({message:"student data added",newStudent})
+})
+
+
+//edit 
+
+app.patch("/student/:id",(req,res)=>{
+
+    const id= parseInt(req.params.id);
+    const index = students.findIndex((std)=>std.id === id);
+
+    if(index==-1){
+        return res.status(404).json("student data not found")
+    }
+
+    students[index] = {...students[index],...req.body}
+
+    res.status(200).json({
+        message:"student data updated suceesfully",
+        student:students[index],
+    })
+})
+
+//delete
+
+app.delete("/student/:id",(req,res)=>{
+
+
+    const id = parseInt(req.params.id)
+
+    // students = students.filter((std)=>std.id !==id);
+
+    // res.status(200).json({message:"stundet data deleted"})
+
+    const index = students.findIndex((std)=>std.id === id)
+
+
+    if(index==-1){
+    
+        return res.status(404).json("student data not found this id")
+    }
+
+    const deleteStudent = students[index];
+
+    students.splice(index,1)
+
+        res.status(404).json({message:"student data deleted",deleteStudent})
+})
+
 
 
 const port = 5000;
